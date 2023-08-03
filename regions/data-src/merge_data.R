e4a2b738f0d36155aea90e18b0c709a3c2644500
Fonty COLO BE <40957618+1017SQD@users.noreply.github.com>
2023-08-03 17:46:48 +0200

    refactor: importion des donnees parquet
    - utilisation de la fonction left_join de dplyr pour la jointure

--- regions/data-src/merge_data.R
+++ regions/data-src/merge_data.R
@@ -6,21 +6,17 @@ library(sf)
 
 # Load data --------------------------------------------------------------------
 
-db_region <- readxl::read_excel("data/tab_region.xls") %>%
-  filter(Libellé != "Ensemble") %>%
+db_region <- arrow::read_parquet("data/tab_region.parquet") %>%
+  filter(Libellé != "Ensemble" & Libellé != "DROM") %>%
   mutate(Libellé = stringi::stri_trans_general(Libellé, id = "latin-ascii"))
 
 gadm <- readRDS("data/gadm36_FRA_1_sf.rds")
 
 # Data Processing --------------------------------------------------------------
 
-gadm <- st_as_sf(gadm)  # Convertis en objet sf
+gadm <- st_as_sf(gadm) # Convertis en objet sf
 gadm <- st_set_crs(gadm, 4326) %>% # Attribue le système de coordonnées géographiques WGS 84
   mutate(NAME_1 = stringi::stri_trans_general(NAME_1, id = "latin-ascii"))
-
-# Remarques : Nous avons pas d'information sur les DROM dans la table gadm36_FRA_1_sf.rds
-# Il est nécessaire de demander au client de scinder la colonne Provence-Alpes-Cote-d'Azur et Corse
-# si celui-ci souhaite afficher des informations sur la Corse sur la carte.
 
 # Remplace "Haut-de-France" par "Hauts-de-France" dans la colonne "Libellé"
 db_region$Libellé <- gsub("Haut-de-France", "Hauts-de-France", db_region$Libellé)
@@ -29,7 +25,7 @@ db_region$Libellé <- gsub("Haut-de-France", "Hauts-de-France", db_region$Libell
 db_region$Libellé <- gsub("Nouvelle Aquitaine", "Nouvelle-Aquitaine", db_region$Libellé)
 
 # Duplique la ligne "Provence-Alpes-Cote-d'Azur et Corse"
-ligne_a_dupliquer <- db_region[13, ]
+ligne_a_dupliquer <- db_region[12, ]
 
 # Remplace "Provence-Alpes-Cote-d'Azur et Corse" par "Provence-Alpes-Cote d'Azur" dans la colonne "Libellé"
 ligne_a_dupliquer[[1]] <- "Provence-Alpes-Cote d'Azur"
@@ -39,11 +35,11 @@ db_region <- rbind(db_region, ligne_a_dupliquer)
 # Remplace "Provence-Alpes-Cote-d'Azur et Corse" par "Corse" dans la colonne "Libellé"
 db_region$Libellé <- gsub("Provence-Alpes-Cote-d'Azur et Corse", "Corse", db_region$Libellé)
 
-# Jointure des deux tables  
-jointure_df <- merge(db_region, gadm, by.x = "Libellé", by.y = "NAME_1")
+# Jointure des deux tables
+jointure_df <- left_join(db_region, gadm, by = c("Libellé" = "NAME_1"))
 
 # Convertis le dataframe en objet spatial sf
 jointure_sf <- st_as_sf(jointure_df)
 
 # Exporte la table en shapefile
-st_write(jointure_sf, "data/tab_region.shp")
+st_write(jointure_sf, dsn = "data/tab_region.shp")
