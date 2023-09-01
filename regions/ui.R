54a14ce65335b73572153bafb7115d1837f769f0
MounaBelaid <belaid.mounaa@gmail.com>
2023-09-01 17:55:04 +0200

    feat: enlever le logo download

--- regions/ui.R
+++ regions/ui.R
@@ -56,11 +56,6 @@ fluidPage(
             src = "logo-generation.png"
           )
         ),
-        tags$img(
-          src = "logo-download.svg",
-          height = "50px",
-          width = "50px"
-        ),
         tags$head(tags$style(".btn{background:#FFFFFF;} .btn{color: #008b99;}; @media print{@page {size: landscape}};")),
         DownloadButton("downloadData", ".xlsx"),
         actionButton("downloadPDF", ".pdf", onclick = "window.print();")
