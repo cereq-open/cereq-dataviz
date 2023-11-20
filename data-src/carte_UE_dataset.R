59936b80dc9af80a6a6c94de15b7af8355628e88
Edmond <edmondnoack@gmail.com>
2023-11-20 08:38:59 +0100

    TPF par autre forme

--- data-src/carte_UE_dataset.R
+++ data-src/carte_UE_dataset.R
@@ -9,8 +9,10 @@ library(sf)
 #Lecture fichier : 
 
 #1- fichier indicateur
-UE <- read_parquet("data/data_carte_UE/Base_UE.parquet")
 
+UE <- readxl::read_excel("data/UE.xlsx")
+
+tx_form <- readxl::read_excel("data/rajout_autre_forme_2.xlsx")
 #2 - lecture du fichier shp pour la carte 
 Monde <- st_read("data/data_carte_UE/CNTR_RG_10M_2020_4326.shp", quiet = TRUE)
 
@@ -23,7 +25,6 @@ Europe <- transform(Europe, tx_form = as.numeric(tx_form),
                     heurstag = as.numeric(heurstag),
                     tx_tpf = as.numeric(tx_tpf),
                     tx_acc1 = as.numeric(tx_acc1))
-
 
 Europe$CNTR_ID <- NULL
 Europe$CNTR_NAME <- NULL
@@ -41,6 +42,9 @@ Europe$secteur <- fct_recode(Europe$secteur,
 Europe$taille <- fct_recode(Europe$taille,
                             "Ensemble des tailles" = "Total")
 
+
+Europe_2 <- sp::merge(Europe , tx_form, by.x = "KEY", by.y = "KEY")
+
 # Création liste pour menus déroulants
 
 Europe_nodupkey <- Europe %>% distinct(secteur, .keep_all = TRUE)
@@ -52,21 +56,24 @@ ensemble_liste_secteur = list('Ensemble des secteurs')
 liste_secteur2 <- c(ensemble_liste_secteur, liste_secteur)
 
 ########################################################################################
-
-
-liste_taille <-  list('Ensemble des tailles',"10 à 49", "49 à 249", "250 ou plus" )
-
-
-
-
-
-
-
+Europe_nodupkey_taille <- Europe %>% distinct(taille, .keep_all = TRUE)
+liste_taille <- na.omit(Europe_nodupkey_taille$taille)
+liste_taille <- as.character( liste_taille )
+liste_taille <- as.list(sort(liste_taille))
+liste_taille[4]<-NULL
+ensemble_liste_taille = list('Ensemble des tailles')
+liste_taille2 <- c(ensemble_liste_taille, liste_taille)
 
 
 
 
 
-saveRDS(Europe, file = "carte_UE/data/Europe.RDS")
+
+
+
+
+
+
+saveRDS(Europe_2, file = "carte_UE/data/Europe.RDS")
 saveRDS(liste_secteur2, file = "carte_UE/data/liste_secteur2.RDS")
-saveRDS(liste_taille, file = "carte_UE/data/liste_taille2.RDS")
+saveRDS(liste_taille2, file = "carte_UE/data/liste_taille2.RDS")
