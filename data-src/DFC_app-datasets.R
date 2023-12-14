84cf5c6f5af38afc27d5cdfb0a5b0a881a1bcf54
Edmond <edmondnoack@gmail.com>
2023-12-14 11:22:28 +0100

    data pour app double menu secteur

---
+++ data-src/DFC_app-datasets.R
@@ -0,0 +1,207 @@
+library(NestedMenu)
+library(shiny)
+library(readr)
+library(readxl)
+library(arrow)
+
+library(tidyverse)
+# Load data --------------------------------------------------------------------
+
+#EFE_1 <-  read_excel("data/EFE_1.xlsx")
+
+EFE_1_large <- read_excel("DFC_app/data/EFE_1_secteur_large.xlsx")
+EFE_1_fin <- read_excel("DFC_app/data/EFE_1_secteur_fin.xlsx")
+EFE_1 <- read_excel("DFC_app/data/EFE_1.xlsx")
+
+
+EFE_1 <- EFE_1 %>% 
+  rename( secteur =lib_secteur)
+
+EFE_1 <- EFE_1 %>% 
+  rename(  taille= lib_taille)
+
+
+
+#FIN
+EFE_1_fin <- EFE_1_fin %>% 
+  rename( secteur =lib_secteur)
+
+EFE_1_fin <- EFE_1_fin %>% 
+  rename(  taille= lib_taille)
+
+EFE_1_fin$secteur[EFE_1_fin$secteur == 'Ensemble'] <- 'Ensemble des secteurs'
+
+EFE_1_fin$tx_acc <- round(EFE_1_fin$tx_acc, digits = 0)
+EFE_1_fin$heurstag <- round(EFE_1_fin$heurstag, digits = 0)
+EFE_1_fin$tx_form <- round(EFE_1_fin$tx_form, digits = 0)
+EFE_1_fin$tx_tpf <- round(EFE_1_fin$tx_tpf, digits = 1)
+EFE_1_fin$heurstag_sal <- round(EFE_1_fin$heurstag_sal, digits = 0)
+EFE_1_fin$tx_courses <- round(EFE_1_fin$tx_courses, digits = 0)
+EFE_1_fin$top1_c5_tx <- round(EFE_1_fin$top1_c5_tx, digits = 0)
+EFE_1_fin$top2_c5_tx <- round(EFE_1_fin$top2_c5_tx, digits = 0)
+EFE_1_fin$top3_c5_tx <- round(EFE_1_fin$top3_c5_tx, digits = 0)
+EFE_1_fin$top1_d3_tx <- round(EFE_1_fin$top1_d3_tx, digits = 0)
+EFE_1_fin$top2_d3_tx <- round(EFE_1_fin$top2_d3_tx, digits = 0)
+EFE_1_fin$top3_d3_tx <- round(EFE_1_fin$top3_d3_tx, digits = 0)
+EFE_1_fin$top1_e1_tx <- round(EFE_1_fin$top1_e1_tx, digits = 0)
+EFE_1_fin$top2_e1_tx <- round(EFE_1_fin$top2_e1_tx, digits = 0)
+EFE_1_fin$top3_e1_tx <- round(EFE_1_fin$top3_e1_tx, digits = 0)
+
+#large
+
+EFE_1_large <- EFE_1_large %>% 
+  rename( secteur =lib_secteur)
+
+EFE_1_large <- EFE_1_large %>% 
+  rename(  taille= lib_taille)
+
+EFE_1_large$secteur[EFE_1_large$secteur == 'Ensemble'] <- 'Ensemble des secteurs'
+
+EFE_1_large$tx_acc <- round(EFE_1_large$tx_acc, digits = 0)
+EFE_1_large$heurstag <- round(EFE_1_large$heurstag, digits = 0)
+EFE_1_large$tx_form <- round(EFE_1_large$tx_form, digits = 0)
+EFE_1_large$tx_tpf <- round(EFE_1_large$tx_tpf, digits = 1)
+EFE_1_large$heurstag_sal <- round(EFE_1_large$heurstag_sal, digits = 0)
+EFE_1_large$tx_courses <- round(EFE_1_large$tx_courses, digits = 0)
+EFE_1_large$top1_c5_tx <- round(EFE_1_large$top1_c5_tx, digits = 0)
+EFE_1_large$top2_c5_tx <- round(EFE_1_large$top2_c5_tx, digits = 0)
+EFE_1_large$top3_c5_tx <- round(EFE_1_large$top3_c5_tx, digits = 0)
+EFE_1_large$top1_d3_tx <- round(EFE_1_large$top1_d3_tx, digits = 0)
+EFE_1_large$top2_d3_tx <- round(EFE_1_large$top2_d3_tx, digits = 0)
+EFE_1_large$top3_d3_tx <- round(EFE_1_large$top3_d3_tx, digits = 0)
+EFE_1_large$top1_e1_tx <- round(EFE_1_large$top1_e1_tx, digits = 0)
+EFE_1_large$top2_e1_tx <- round(EFE_1_large$top2_e1_tx, digits = 0)
+EFE_1_large$top3_e1_tx <- round(EFE_1_large$top3_e1_tx, digits = 0)
+vec_secteur <- unique(EFE_1_large$secteur)
+
+
+
+EFE_1$secteur[EFE_1$secteur == 'Ensemble'] <- 'Ensemble des secteurs'
+
+
+EFE_1_nodupkey <- EFE_1 %>% distinct(secteur, .keep_all = TRUE)
+ensemble = list('Ensemble')
+liste_secteur <- as.list(sort(EFE_1_nodupkey$secteur))
+liste_secteur[17] <- NULL
+liste_secteur2 <- c(ensemble, liste_secteur)
+
+
+EFE_1_nodupkey_taille <- EFE_1 %>% distinct(taille, .keep_all = TRUE)
+
+ensemble_liste_taille = list('Ensemble')
+liste_taille <- as.list((EFE_1_nodupkey_taille$taille))
+liste_taille[1] <- NULL
+liste_taille2 <- c(ensemble_liste_taille, liste_taille)
+
+
+source <- paste0(
+  '<span style="color:#008B99;">Sources : </span>',
+  "Source : Céreq-Dares-France compétences, Enquête Formation Employeur – européenne (EFE-e, Données 2020)"
+)
+
+
+caption_part_1 <- paste0(
+  '<span style="color:#008B99;">Champ : </span>',
+  "Ensemble des entreprises de 1 salarié et plus du secteur privé (Hors activités des ménages et extraterritoriales)",
+  "<br>",
+  source
+)
+
+
+EFE_1$secteur  <- fct_relevel(EFE_1$secteur, c(
+  "Service ensemble"   ,                                         "Activités auxiliaires de services financiers et d'assurance",
+  "Activités créatives, artistiques et de spectacle"    ,        "Activités de location"    ,                                  
+  "Activités des organisations associatives" ,                   "Activités des services financiers"   ,                       
+  "Activités immobilières"              ,                        "Activités juridiques et comptables"  ,                       
+  "Administration publique et santé" ,                           "Agriculture, sylviculture et pêche"    ,                    
+  "Commerce ensemble"      ,                                     "Cockefaction et raffinage"      ,                            
+  "Commerce de détail"      ,                                    "Commerce de gros"          ,
+  "Commerce et réparation d'automobiles"     ,                   "Construction"         ,                                      
+  "Edition, Production de films cinématographiques"    ,         "Enseignement"          ,                                     
+  "Fabrication de meubles"                          ,           
+  "Industrie ensemble"                                       ,   "Fabrication de produits informatiques"               ,       
+  "Fabrication de textiles"                                   ,  "Hébergement, Restauration"                            ,      
+  "Industrie automobile"                                       , "Industrie du papier et du carton"                      ,     
+  "Industries alimentaire"                                      ,"Industries extractives"                                 ,    
+  "Métallurgie"                                                 ,"Production et distribution d'électricité"  ,
+  "Transports"  ,"Ensemble des secteurs"  ))  
+
+EFE_1$secteur_large  <- fct_relevel(EFE_1$secteur_large, c( 
+  "Ensemble",  "Commerce" , "Industrie" , "Service"  ))
+
+
+
+
+
+
+
+
+EFE_1_fin$taille <- fct_relevel(EFE_1_fin$taille, c("moins de 50", "plus de 50" , "Ensemble" ))
+EFE_1_large$taille <- fct_relevel(EFE_1_large$taille, c("1 à 3 salariés", "4 à 9 salariés", "10 à 19 salariés","20 à 49 salariés","50 à 249 salariés","250 à 499 salariés", "500 à 999 salariés", "1000 salariés et plus", "Ensemble" ))
+        
+
+EFE_1_fin$secteur  <- fct_relevel(EFE_1_fin$secteur, c(
+  "Service ensemble"   ,                                         "Activités auxiliaires de services financiers et d'assurance",
+  "Activités créatives, artistiques et de spectacle"    ,        "Activités de location"    ,                                  
+  "Activités des organisations associatives" ,                   "Activités des services financiers"   ,                       
+  "Activités immobilières"              ,                        "Activités juridiques et comptables"  ,                       
+  "Administration publique et santé" ,                           "Agriculture, sylviculture et pêche"    ,                    
+  "Commerce ensemble"      ,                                     "Cockefaction et raffinage"      ,                            
+  "Commerce de détail"      ,                                    "Commerce de gros"          ,
+  "Commerce et réparation d'automobiles"     ,                   "Construction"         ,                                      
+  "Edition, Production de films cinématographiques"    ,         "Enseignement"          ,                                     
+  "Fabrication de meubles"                          ,           
+  "Industrie ensemble"                                       ,   "Fabrication de produits informatiques"               ,       
+  "Fabrication de textiles"                                   ,  "Hébergement, Restauration"                            ,      
+  "Industrie automobile"                                       , "Industrie du papier et du carton"                      ,     
+  "Industries alimentaire"                                      ,"Industries extractives"                                 ,    
+  "Métallurgie"                                                 ,"Production et distribution d'électricité"  ,
+  "Transports"  ,"Ensemble des secteurs"  ))  
+
+EFE_1_large$secteur_large  <- fct_relevel(EFE_1_large$secteur_large, c( 
+  "Service ensemble" , "Industrie ensemble" ,  "Commerce ensemble"  , "Ensemble des secteurs" ))
+
+
+
+EFE_1_large$secteur  <- fct_relevel(EFE_1_large$secteur, c( 
+  "Service ensemble" , "Industrie ensemble" ,  "Commerce ensemble"  , "Ensemble des secteurs" ))
+
+
+
+EFE_1$secteur_large  <- fct_relevel(EFE_1$secteur_large, c( 
+  "Ensemble", "Commerce"  ,"Industrie" , "Service" ))
+
+
+
+colors_fin<-setNames(c("#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB",
+                       "#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#046B76","#7FC4CB",
+                       "#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB",
+                       "#7FC4CB","#046B76"),unique(EFE_1$secteur))
+
+colors_large<-setNames(c("#7FC4CB","#7FC4CB","#046B76","#7FC4CB"),unique(EFE_1_large$secteur))
+
+
+
+saveRDS(EFE_1_large, file = "DFC_app/data/EFE_1_large.RDS")
+saveRDS(EFE_1_fin, file = "DFC_app/data/EFE_1_fin.RDS")
+saveRDS(liste_secteur2, file = "DFC_app/data/liste_secteur2.RDS")
+saveRDS(liste_taille2, file = "DFC_app/data/liste_taille2.RDS")
+
+
+saveRDS(source, file = "DFC_app/data/source.RDS")
+saveRDS(caption_part_1, file = "DFC_app/data/caption_part_1.RDS")
+
+
+
+
+saveRDS(colors2, file = "DFC_app/data/colors2.RDS")
+saveRDS(colors, file = "DFC_app/data/colors.RDS")
+
+saveRDS(colors_large, file = "DFC_app/data/colors_large.RDS")
+
+
+
+
+
+
+
