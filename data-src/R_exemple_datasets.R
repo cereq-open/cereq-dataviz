b5d16e148217ea1c735356852e8b0c1c032833f9
Edmond <edmondnoack@gmail.com>
2023-12-22 10:09:45 +0100

    note

---
+++ data-src/R_exemple_datasets.R
@@ -0,0 +1,337 @@
+EFE_1 <- read_excel("data/EFE_1.xlsx")
+
+
+
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5ab'] <- 'Technologie de l’information et de la communication'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5c'] <- 'Management'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5d'] <- 'Travail en équipe'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5e'] <-  'Commercial'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5f'] <- 'Résolution de problèmes'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5g'] <- 'Administratif'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5h'] <- 'Langues étrangères'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5i'] <- 'Techniques spécifiques à un métier'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5j'] <- 'Communication écrite ou orale'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5k'] <- 'Savoir lire, écrire, compter'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5l'] <- 'Transition écologique'
+EFE_1$top1_c5[EFE_1$top1_c5 == 'C5m'] <- 'Autres compétences'
+
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5ab'] <- 'Technologie de l’information et de la communication'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5c'] <- 'Management'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5d'] <- 'Travail en équipe'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5e'] <-  'Commercial'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5f'] <- 'Résolution de problèmes'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5g'] <- 'Administratif'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5h'] <- 'Langues étrangères'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5i'] <- 'Techniques spécifiques à un métier'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5j'] <- 'Communication écrite ou orale'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5k'] <- 'Savoir lire, écrire, compter'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5l'] <- 'Transition écologique'
+EFE_1$top2_c5[EFE_1$top2_c5 == 'C5m'] <- 'Autres compétences'
+
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5ab'] <- 'Technologie de l’information et de la communication'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5c'] <- 'Management'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5d'] <- 'Travail en équipe'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5e'] <-  'Commercial'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5f'] <- 'Résolution de problèmes'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5g'] <- 'Administratif'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5h'] <- 'Langues étrangères'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5i'] <- 'Techniques spécifiques à un métier'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5j'] <- 'Communication écrite ou orale'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5k'] <- 'Savoir lire, écrire, compter'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5l'] <- 'Transition écologique'
+EFE_1$top3_c5[EFE_1$top3_c5 == 'C5m'] <- 'Autres compétences'
+
+
+
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3a'] <- 'Effort de formation approprié aux besoins'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3f'] <- "Priorité donnée à l'alternace"
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top1_d3[EFE_1$top1_d3 == 'd3i'] <- 'Crise sanitaire'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3i'] <- 'Autres raisons'
+
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3a'] <- 'Effort de formation approprié aux besoins'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3f'] <- "Priorité donnée à l'alternace"
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3i'] <- 'Crise sanitaire'
+EFE_1$top2_d3[EFE_1$top2_d3 == 'd3i'] <- 'Autres raisons'
+
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3a'] <- 'Effort de formation approprié aux besoins'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3f'] <- "Priorité donnée à l'alternace"
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3i'] <- 'Crise sanitaire'
+EFE_1$top3_d3[EFE_1$top3_d3 == 'd3j'] <- 'Autres raisons'
+
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1a'] <- "Qualifications et compétences appropriées aux besoins de l'entreprise"
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1f'] <- "Priorité donnée à la formation initiale"
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top3_e1[EFE_1$top3_e1 == 'e1i'] <- 'Crise sanitaire'
+
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1a'] <- "Qualifications et compétences appropriées aux besoins de l'entreprise"
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1f'] <- "Priorité donnée à la formation initiale"
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top1_e1[EFE_1$top1_e1 == 'e1i'] <- 'Crise sanitaire'
+
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1a'] <- "Qualifications et compétences appropriées aux besoins de l'entreprise"
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1b'] <- 'Recrutement de personnes ayant les qualifications et compétences requises'
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1c'] <- "Difficulté d'évaluation des besoins en formation"
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1d'] <-  'Absence de cours ou stages adaptés aux besoins'
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1e'] <- 'Coûts des cours ou stages trop élevés'
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1f'] <- "Priorité donnée à la formation initiale"
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1g'] <- 'Efforts de formation importants antérieurment'
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1h'] <- 'Charge de travail trop lourde ou manque de temps'
+EFE_1$top2_e1[EFE_1$top2_e1 == 'e1i'] <- 'Crise sanitaire'
+
+
+
+
+EFE_1$taille[EFE_1$taille == '1 à 10 salariés'] <- '1 à 10'
+EFE_1$taille[EFE_1$taille == '11 à 49 salariés'] <- '11 à 49'
+EFE_1$taille[EFE_1$taille == '50 à 299 salariés'] <- '50 à 299'
+EFE_1$taille[EFE_1$taille == '300 à 999 salariés'] <- '300 à 999'
+EFE_1$taille[EFE_1$taille == '1000 salariés et plus'] <- '1000 et plus'
+EFE_1$secteur[EFE_1$secteur == 'Ensemble'] <- 'Ensemble des secteurs'
+
+EFE_1$tx_acc <- round(EFE_1$tx_acc, digits = 0)
+EFE_1$heurstag <- round(EFE_1$heurstag, digits = 0)
+EFE_1$tx_form <- round(EFE_1$tx_form, digits = 0)
+EFE_1$tx_autres <- round(EFE_1$tx_autres, digits = 0)
+EFE_1$heurstag_sal <- round(EFE_1$heurstag_sal, digits = 0)
+EFE_1$tx_courses <- round(EFE_1$tx_courses, digits = 0)
+
+EFE_1$top1_c5_tx <- round(EFE_1$top1_c5_tx, digits = 0)
+EFE_1$top2_c5_tx <- round(EFE_1$top2_c5_tx, digits = 0)
+EFE_1$top3_c5_tx <- round(EFE_1$top3_c5_tx, digits = 0)
+EFE_1$top1_d3_tx <- round(EFE_1$top1_d3_tx, digits = 0)
+EFE_1$top2_d3_tx <- round(EFE_1$top2_d3_tx, digits = 0)
+EFE_1$top3_d3_tx <- round(EFE_1$top3_d3_tx, digits = 0)
+EFE_1$top1_e1_tx <- round(EFE_1$top1_e1_tx, digits = 0)
+EFE_1$top2_e1_tx <- round(EFE_1$top2_e1_tx, digits = 0)
+EFE_1$top3_e1_tx <- round(EFE_1$top3_e1_tx, digits = 0)
+
+
+vec_secteur <- unique(EFE_1$secteur)
+
+
+EFE_1$ns_txacc= case_when(
+  EFE_1$ns_txacc==1~"NS",
+  TRUE ~ paste0(as.character(EFE_1$tx_acc), " %"))
+
+
+EFE_1$ns_txform= case_when(
+  EFE_1$ns_txform==1~"NS",
+  TRUE ~ paste0(as.character(EFE_1$tx_form), " %"))
+
+
+EFE_1$ns_txcourses= case_when(
+  EFE_1$ns_txcourses==1~"NS",
+  TRUE ~  paste0(as.character(EFE_1$tx_courses), " %"))
+
+EFE_1$ns_txautres= case_when(
+  EFE_1$ns_txautres==1~"NS",
+  TRUE ~  paste0(as.character(EFE_1$tx_autres), " %"))
+
+EFE_1$ns_heurstag=paste0(as.character(EFE_1$heurstag), " h")
+
+EFE_1$ns_heurstag_sal=  paste0(as.character(EFE_1$heurstag_sal), " h")
+
+
+
+
+
+
+view(EFE_1)
+
+# Couleurs des barplots
+
+
+
+col=rep("#7FC4CB",6)
+colors2<-setNames(c("#046B76","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB","#7FC4CB"),unique(EFE_1$secteur))
+colors <-setNames(c("#046B76",col),unique(EFE_1$secteur))
+
+
+
+plot_barchart <- function(df, y_col,y_col2, caption_texte, titre = NULL) {
+  DT <- concat_value(df, y_col)
+  ggplot(data = DT, aes(x = taille, y = !!sym(y_col), fill = as.factor(secteur), tooltip =tooltip_value, data_id = taille )) + geom_bar_interactive(stat = "identity", position = "dodge")  +
+    geom_text(aes(x= taille, label = !!sym(y_col2)),  position = position_dodge(width = 0.9),vjust= -0.8, size= 2.5, color = "#7FC4CB"    ,family = "Arimo") + scale_fill_manual(values = colors2) + 
+    labs(caption = caption_texte, title = titre,x = "Taille de l'entreprise en nombre de salariés") + 
+    scale_x_discrete(labels = c('1 à 10','11 à 49', '50 à 299','300 à 999','1000 et +',       expression(bold("Ensemble")) )) +
+theme_set(
+  theme(
+    line = element_line(colour = "black", linewidth = 0.1),
+    title = element_text(family = "Arimo", size = 6),
+    text = element_text(size = 11, family = "Arimo"),
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(color = "#008B99", size = 8, hjust = 0.4, vjust = 5),
+    axis.text.y = element_blank(),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(color = "#008B99", size = 10, vjust = 4),
+    plot.title = element_textbox_simple(hjust = 0, size = 17, color = "#008B99"),
+    legend.title = element_blank(),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    plot.caption.position = "plot",
+    legend.position = "top",
+    legend.justification = "center",
+    plot.caption = element_textbox_simple( hjust = 100, color = "#808080", size = 10 , margin = margin(t = -1)) 
+  )
+)
+
+}
+
+EFE_1_nodupkey <- EFE_1 %>% distinct(secteur, .keep_all = TRUE)
+
+
+EFE_1$secteur  <- fct_relevel(EFE_1$secteur, c( 
+  
+
+  "Agriculture, sylviculture, pêche",
+  "Industrie",
+  "Construction",
+  "Commerce, transports et services divers",
+  "Enseignement, santé et action sociale",
+
+  "Ensemble des secteurs"))
+
+EFE_1_nodupkey_taille <- EFE_1 %>% distinct(taille, .keep_all = TRUE)
+
+ensemble_liste_taille = list('Ensemble')
+liste_taille <- as.list((EFE_1_nodupkey_taille$taille))
+liste_taille[1] <- NULL
+liste_taille2 <- c(ensemble_liste_taille, liste_taille)
+
+EFE_1$taille  <- fct_relevel(EFE_1$taille, c( 
+  '1 à 10','11 à 49', '50 à 299','300 à 999','1000 et plus', 'Ensemble'))
+
+
+
+
+view(EFE_1)
+
+write.xlsx(EFE_1,"EFE_1_clair.xlsx", encoding="UTF-8", sep=";")
+
+ensemble = list('Ensemble des secteurs')
+liste_secteur <- as.list(sort(EFE_1_nodupkey$secteur))
+
+liste_secteur[5] <- NULL
+liste_secteur2 <- c(ensemble, liste_secteur)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Couleurs des barplots
+source <- paste0(
+  '<span style="color:#008B99;">Sources : </span>',
+  "Source : Céreq-Dares-France compétences, Enquête Formation Employeur – européenne (EFE-e, Données 2020)"
+)
+
+
+caption_part_1 <- paste0(
+  '<span style="color:#008B99;">Champ : </span>',
+  "Ensemble des entreprises de 1 salarié et plus du secteur privé (Hors activités des ménages et extraterritoriales)",
+  "<br>",
+  source
+)
+
+
+
+
+#TOOLTIP
+concat_value <- function(df, nom_colonne) {
+  
+  if (nom_colonne != "heurstag" & nom_colonne!="heurstag_sal" )
+    df["taux_str"] <- paste0(df[[nom_colonne]], "%")
+  else {
+    df["taux_str"] <- paste0(df[[nom_colonne]], "h")
+  }
+  
+  
+  df[["tooltip_value"]] <- paste0(df[["secteur"]], " : ", df[["taux_str"]] )
+
+  
+  
+  return(df)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+saveRDS(plot_barchart, file = "Rexemple/data/plot_barchart.RDS")
+
+
+saveRDS(EFE_1, file = "Rexemple/data/EFE_1.RDS")
+
+saveRDS(liste_secteur2, file = "Rexemple/data/liste_secteur2.RDS")
+saveRDS(liste_taille2, file = "Rexemple/data/liste_taille2.RDS")
+
+
+saveRDS(source, file = "Rexemple/data/source.RDS")
+saveRDS(caption_part_1, file = "Rexemple/data/caption_part_1.RDS")
+saveRDS(concat_value, file = "Rexemple/data/concat_value.RDS")
+
+
+
+saveRDS(colors, file = "Rexemple/data/colors.RDS")
+saveRDS(colors2, file = "Rexemple/data/colors2.RDS")
+
+
+
