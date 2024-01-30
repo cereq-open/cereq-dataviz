379f3f13f58bd48fa25417c5b3ac3b60a67e97bc
Edmond <edmondnoack@gmail.com>
2024-01-30 09:55:54 +0100

    modif tooltip

--- carte_UE/global.R
+++ carte_UE/global.R
@@ -27,6 +27,31 @@ Europe <- readRDS("data/Europe.RDS")
 liste_secteur2 <- readRDS("data/liste_secteur2.RDS")
 liste_taille2 <- readRDS("data/liste_taille2.RDS")
 
+Europe$taille <- fct_recode(Europe$taille,
+                             "10 à 49 salariés" = "10 à 49")
+
+
+Europe$taille <- fct_recode(Europe$taille,
+                            "50 à 249 salariés" = "50 à 249")
+
+Europe$taille <- fct_recode(Europe$taille,
+                            "250 salariés ou plus" = "250 ou plus")
+
+
+#TOOLTIP
+concat_value <- function(df, nom_colonne) {
+  if (nom_colonne != "heurstag" & nom_colonne!="heurstag_sal") {
+    df["taux_str"] <- paste0(df[[nom_colonne]], "%")
+  } else {
+    df["taux_str"] <- paste0(df[[nom_colonne]], "h")
+  }
+  
+  df[["tooltip_value"]] <- paste0(df[["secteur"]], " : ", df[["taux_str"]], df[["taille"]] )
+  
+  return(df)
+}
+
+
 ###########################################################################################
 token<-"8C2gU8pSutHVOkE3id0L7olcMCYjc2Aoh3GdmmneYDBw6bX4m1gBzw9t3JMM0EU9"
 
